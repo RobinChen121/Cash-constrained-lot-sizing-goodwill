4e67dce70f6094c8bc76000bdec073b99d7c0971
RobinChen121 <40953071+RobinChen121@users.noreply.github.com>
2019-02-21 12:36:21 +0800

    revise some files

--- ComputeMkn.m
+++ ComputeMkn.m
@@ -8,8 +8,8 @@ function [finalXMn, finalB, satDemand, finalIniB, finalIniW, finalW] = ComputeMk
 % Parameters:
 % xMn: ordering plan in an ordering cycle from period m to period n
 % lastX: an array, previous optimal ordering plan
-% thisIniB: initial cash for this ordering round
-% thisIniW: initial lost sale for this ordering round
+% thisIniB: an array, initial cash for each ordering cycle in this ordering round
+% thisIniW: an array, initial lost sale for each ordering cycle in this ordering round
 % lastB: end-of period cash in the final period of previous ordering plan
 % lastW: lost sale quantity in the final period of previous ordering plan
 % lastSatDemand: an array, satifying quantity of the previous ordering plan
@@ -36,7 +36,73 @@ function [finalXMn, finalB, satDemand, finalIniB, finalIniW, finalW] = ComputeMk
 % time: 2019-02-19, 17:16
 % ***************************************************************************                                
                                 
+%% initial setting, the ordering round: from period m to period n, include two ordering cycles: m ~ k, k + 1 ~ n
+finalB = lastB;
+options = optimset('TolFun', 1e-6, 'MaxIter', 20);
+n = length(p);
+m = 1;
+iniB = thisIniB(1);
+iniW = thisIniW(1);
+finalXMn = xMn;
+[~, orderIndex] = find(xMn == 1);                                
                                 
-                                
+%% calling ComputefAb to get f A b
+[f, A, b] = ComputefAb(xMn, iniB, iniW, lastW, d(m : n), p(m : n), c(m : n), s(m : n), h(m : n), beta, zeros(1, n - m + 1),...
+                       needLossless, thisLoanLength, ratePay);
+lb = zeros(1,n); ub = d(1 : n);
+
+%% calling linprog to compute satDemand
+[satDemand, fval, exitflag] = linprog(f, A, b, [], [], lb, ub, [], options);
+
+% if no solution, relax the effective demand constraint
+if exitflag ~= 1
+    [satDemand, ~, exitflag1] = linprog(f, A(1 : n - 1 + sum(xMn), :), b(1 : n - 1 + sum(xMn), :), [], [], lb, ub, [], options);  
+    if exitflag1 ~= 1
+        satDemand = zeros(1, n - m + 1)';
+    end
+end
+
+%% check whether cash is increasing or not, and update
+orderLastLength = diff(orderIndex);
+orderLastLength(end + 1) = n - m - orderIndex(end) + 2; 
+finalIniW = zeros(1, length(thisIniW));
+finalIniB = zeros(1, length(thisIniW));
+if needLossless == 0 && thisLoanLength == n % need to pay back loan in period n
+    lastB = lastB - ratePay; 
+end
+if exitflag ~= 1 || finalB < lastB - 1e-2
+    finalB = lastB;
+    finalIniB = thisIniB;
+    finalIniW = thisIniW;
+    if needLossless == 0 % two situations
+        satDemand = [lastSatDemand; 0]; finalXMn = [lastX,0];
+    else
+        satDemand = lastSatDemand; finalXMn = lastX;
+    end 
+else
+    finalIniB(1) = iniB;
+    % recompute finalIniW when solution is like 0, 0, 0, 1
+    if orderIndex(1) > 1
+        [~,~,wFlow] = ComputeEd(zeros(1, orderIndex(1) - 1), d(1 : orderIndex(1) - 1), iniW, beta);
+        finalIniW(1) = wFlow(end);
+    else
+        finalIniW(1) = iniW; 
+    end
+    for xNum = 1 : sum(xMn) - 1
+        kStart = orderIndex(xNum); % starting period index of cycle m~k
+        kLast = kStart + orderLastLength(xNum) - 1;
+        finalIniB(xNum + 1) = -A(kLast,:) * satDemand - s(1 : kLast) * xMn(1 : kLast)' + iniB; 
+        if thisLoanLength > 0 && orderIndex(xNum + 1) > thisLoanLength % need to pay back loan in this situation
+            finalIniB(xNum + 1) = finalIniB(xNum+1) - ratePay;
+        end
+    end 
+end
+[~, ~, w] = ComputeEd(satDemand, d, iniW, beta);
+finalW = w(n);
+for xNum = 1 : sum(xMn) - 1
+    kStart = orderIndex(xNum);
+    kLast = kStart + orderLastLength(xNum) - 1;
+    finalIniW(xNum + 1) = w(kLast);
+end
 
 end
